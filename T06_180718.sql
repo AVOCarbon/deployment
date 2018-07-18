970624f5a3d800c10d888deb4e00229cd14339cd
Andy K <andy.h.kwok@gmail.com>
2018-07-18 15:24:25 +0200

    Add files via upload
    extra columns for T06_Machines for prodReporter

---
+++ T06_180718.sql
@@ -0,0 +1,17 @@
+﻿/*
+18/07/18
+adding extra columns for prodreporter
+*/
+
+
+ALTER TABLE dw."T06_Machines"
+ADD COLUMN "CapacityPerWeek" integer,
+ADD COLUMN  "ProductGroup" character varying,
+ADD COLUMN  controler_manufacturer character varying,
+ADD COLUMN  controler_name character varying,
+ADD COLUMN  controler_type character varying,
+ADD COLUMN  controler_voltage character varying,
+ADD COLUMN  controler_output_cyclecount character varying,
+ADD COLUMN  controler_output_defectivepart character varying,
+ADD COLUMN  controler_output_failure character varying,
+ADD COLUMN  controler_output_other character varying
